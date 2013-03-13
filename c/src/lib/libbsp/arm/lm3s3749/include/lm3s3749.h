0194aa1f0d6beb4c0f063eae2a35e3e815240e74
Eugeniy Meshcheryakov <eugen@debian.org>
2013-03-13 01:18:43 +0100

    Initial port to LM3S3749, based on lm3s3749

---
+++ c/src/lib/libbsp/arm/lm3s3749/include/lm3s3749.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2011 Sebastian Huber.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Obere Lagerstr. 30
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifndef LIBBSP_ARM_LM3S3749_LM3S3749_H
+#define LIBBSP_ARM_LM3S3749_LM3S3749_H
+
+#define LM3S3749_UART_0_BASE 0x4000c000
+#define LM3S3749_UART_1_BASE 0x4000d000
+#define LM3S3749_UART_2_BASE 0x4000e000
+
+#endif /* LIBBSP_ARM_LM3S3749_LM3S3749_H */
