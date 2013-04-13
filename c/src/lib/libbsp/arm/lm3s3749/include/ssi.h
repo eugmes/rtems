6f0f4851202386b1f7bcfdc5cad6c9ef54d1a1b2
Eugeniy Meshcheryakov <eugen@debian.org>
2013-04-13 21:41:26 +0200

    Add missing include file

---
+++ c/src/lib/libbsp/arm/lm3s3749/include/ssi.h
@@ -0,0 +1,16 @@
+#ifndef LIBBSP_ARM_LM3S3749_SSI_H
+#define LIBBSP_ARM_LM3S3749_SSI_H
+#include <rtems/libi2c.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern rtems_libi2c_bus_t * const lm3s3749_ssi_0;
+extern rtems_libi2c_bus_t * const lm3s3749_ssi_1;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
